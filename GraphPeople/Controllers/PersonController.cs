2b3bb55cf7699aad56e23641ae6f3f52d7c9f12f
Hunt Matt <Matt.Hunt@aldautomotive.com>
2017-03-31 13:03:04 +0100

    Init

---
+++ GraphPeople/Controllers/PersonController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using GraphPeople.Models;
+using GraphPeople.ViewModels.Person;
+using Neo4jClient;
+
+namespace GraphPeople.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        private readonly IGraphClient _graphClient;
+
+        public PersonController()
+        {
+            _graphClient = new GraphClient(new Uri("http://localhost:7474/db/data"), "username", "password");
+            //_graphClient.Connect();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(PersonViewModel model)
+        {
+            Person personToSave = new Person
+            {
+                Id = model.Id == Guid.Empty ? Guid.NewGuid() : model.Id,
+                Name = model.Name
+            };
+
+            // Creates if not exists
+            _graphClient.Cypher
+                .Merge("(person:Person { Id: {id} })")
+                .OnCreate()
+                .Set("person = {personToSave}")
+                .WithParams(new
+                {
+                    id = personToSave.Id,
+                    personToSave = personToSave
+                })
+                .ExecuteWithoutResults();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GraphPeople/ViewModels/Person/PersonViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GraphPeople.ViewModels.Person
+{
+    public class PersonViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
